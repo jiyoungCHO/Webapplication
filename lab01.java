10a12fb1a7de82e364ac587a4c898cb4aece6def
jiyoungCHO <osn98119@hanyang.ac.kr>
2016-09-09 16:51:13 +0900

    New Porject

---
+++ lab01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class lab01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = new Scanner(System.in).nextInt();
+	
+		
+		if (number == 1) {
+			System.out.print("wldud2490@gmail.com");
+		}
+		
+		if (number == 2) {
+			System.out.print("010-5384-2490");
+		}
+			
+		}
+		
+
+	}
+	
+
